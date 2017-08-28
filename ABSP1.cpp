b36ff520d274eea225478f98d7520a281357e4ae
singlasahil221 <singlasahil221@users.noreply.github.com>
2017-08-28 08:15:05 +0530

    Create ABSP1.cpp

---
+++ ABSP1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n,a[100005],sum=0;
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            sum+=(2*i+1-n)*a[i];
+        }
+        cout<<abs(sum)<<"\n";
+    }
+    return 0;
+}
